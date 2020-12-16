5f1cdd8064fcf5762bde6d3d80582322126fe691
VivianaVM01 <76130124+VivianaVM01@users.noreply.github.com>
2020-12-16 15:36:29 -0800

    Add files via upload

---
+++ Arreglo vectorial 3x3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+int x,y, A[4][4];
+
+srand(time(NULL));
+printf("Tu matriz de números aleatorios es: \n");
+for(x=0; x<4; x++){
+for(y=0; y<4; y++){
+A[x][y] = rand()%5;
+printf("%d \t", A[x][y]);
+}
+printf("\n");
+}
+}
