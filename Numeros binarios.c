6f26461f8ad6d1d520eda0812b29a49d77528182
VivianaVM01 <76130124+VivianaVM01@users.noreply.github.com>
2020-12-16 15:25:57 -0800

    Add files via upload

---
+++ Numeros binarios.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+int Binario[100];
+int i=0, j=0;
+int numero;
+printf ("Dame el numero: ");
+scanf ("%d",&numero);
+for (i=100; i>0; i--) {
+Binario[i]=numero%2;
+numero=numero/2;
+}
+for (i=1; i<=100; i++) {
+if (Binario [i]==1){
+for (j=i; j<=100; j++ ){
+printf ("%d\n", Binario[j]);
+}
+break;
+}
+}
+}
